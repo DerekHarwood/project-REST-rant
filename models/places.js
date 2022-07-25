6b3cc660bde1e6907a044880c250d686e3423195
DerekHarwood <101199163+DerekHarwood@users.noreply.github.com>
2022-07-25 19:23:04 -0400

    Create places.js

---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'Finnegans Pub',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Irish Cuisine, Beer',
+    pic: '/images/Finnegans-Pub.jpg'
+}, {
+    name: 'Cozy Bean Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/Cozy-Bean-Cafe.jpg'
+}]
