db6568976a28e2cf2bb0490bc23427762391ccd8
DerekHarwood <101199163+DerekHarwood@users.noreply.github.com>
2022-07-25 11:54:10 -0400

    Update error404.jsx

--- views/error404.jsx
+++ views/error404.jsx
@@ -1,15 +1,28 @@
-const React = require('react')
-const Def = require('./default')
+const React = require("react");
+const Def = require("./default");
 
-function error404 () {
-    return (
-      <Def>
-          <main>
-              <h1>404: PAGE NOT FOUND</h1>
-              <p>Oops, sorry, we can't find this page!</p>
-          </main>
-      </Def>
-    )
-  }  
+function error404() {
+  return (
+    <Def>
+      <main>
+        <h1>404: PAGE NOT FOUND</h1>
+        <p>Oops, sorry, we can't find this page!</p>
+        <div>
+          <img src="/images/brain-404.jpg" alt="Brain has many tabs open." />
+          <div>
+            Photo by{" "}
+            <a href="https://unsplash.com/@thatsherbusiness?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+              That's Her Business
+            </a>{" "}
+            on{" "}
+            <a href="https://unsplash.com/s/photos/quotes?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+              Unsplash
+            </a>
+          </div>
+        </div>
+      </main>
+    </Def>
+  );
+}
 
-module.exports = error404
+module.exports = error404;
