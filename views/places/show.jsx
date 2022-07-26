6f807d5d9f61962a736e43c0dc7004c87ba9ce0d
DerekHarwood <101199163+DerekHarwood@users.noreply.github.com>
2022-07-25 23:13:49 -0400

    Update show.jsx

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,10 +2,22 @@ const React = require("react");
 const Def = require("../default");
 
 function show(data) {
+  let comments = <h3 className="inactive">No comments yet!</h3>;
+  let rating = <h3 className="inactive">Not yet rated</h3>;
+  rating = <h3></h3>;
+  comments = <h3></h3>;
   return (
     <Def>
       <main>
         <h1>{data.place.name}</h1>
+        <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+          Edit
+        </a>
+        <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+          <button type="submit" className="btn btn-danger">
+            Delete
+          </button>
+        </form>
       </main>
     </Def>
   );
