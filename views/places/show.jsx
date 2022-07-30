9d1333e892005b0d0a9b8ee3cebf166b76719706
DerekHarwood <101199163+DerekHarwood@users.noreply.github.com>
2022-07-29 23:17:47 -0400

    Update show.jsx

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,6 +1,22 @@
 const React = require("react");
 const Def = require("../default");
 
+function new_form(data) {
+  let message = "";
+  if (data.message) {
+    message = <h4 className="alert-danger">{data.message}</h4>;
+  }
+  return (
+    <Def>
+      <main>
+        <h1>Add a New Place</h1>
+        {message}
+      </main>
+    </Def>
+  );
+}
+
+module.exports = new_form;
 function show(data) {
   let comments = <h2 className="inactive">Comments</h2>;
   let rating = <h2 className="inactive">Rating</h2>;
@@ -9,15 +25,27 @@ function show(data) {
   return (
     <Def>
       <main>
-        <h1>{data.place.name}</h1>
-        <a href={`/places/${data.id}/edit`} className="btn btn-warning">
-          Edit
-        </a>
-        <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
-          <button type="submit" className="btn btn-danger">
-            Delete
-          </button>
-        </form>
+        <div className="row">
+          <div className="col-sm-6">
+            <img src={data.place.pic} alt={data.place.name} />
+            <h3>
+              Located in {data.place.city}, {data.place.state}
+            </h3>
+          </div>
+          <div className="col-sm-6">
+            <h2>Description</h2>
+            <h3>{data.place.showEstablished()}</h3>
+            <h4>Serving {data.place.cuisines}</h4>
+          </div>
+          <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+            Edit
+          </a>
+          <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+            <button type="submit" className="btn btn-danger">
+              Delete
+            </button>
+          </form>
+        </div>
       </main>
     </Def>
   );
