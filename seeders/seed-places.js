d80ffa03b2fe87d425f384682d8083010e8b7ec8
DerekHarwood <101199163+DerekHarwood@users.noreply.github.com>
2022-07-29 23:17:40 -0400

    Create seed-places.js

---
+++ seeders/seed-places.js
@@ -0,0 +1,26 @@
+const db = require('./models')
+
+db.Place.create([{
+    name: 'Finnegans Pub',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Irish Cuisine, Beer',
+    pic: '/images/Finnegans-Pub.jpg',
+    founded: 1989
+}, {
+    name: 'Cozy Bean Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/Cozy-Bean-Cafe.jpg',
+    founded: 2020
+}])
+.then(() => {
+    console.log('Success!')
+    process.exit()
+})
+.catch(err => {
+    console.log('Failure!', err)
+    process.exit()
+})
+
